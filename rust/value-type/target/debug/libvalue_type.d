d6730c2cc7155b238d7742fad55821651b0ffc5f
jjy <knight09_26@hotmail.com>
2018-01-02 20:43:57 +0900

    rust 2 day

---
+++ rust/value-type/target/debug/libvalue_type.d
@@ -0,0 +1,1 @@
+C:\Users\jjy\server\rust\value-type\target\debug\libvalue_type.rlib: C:\Users\jjy\server\rust\value-type\src\lib.rs
---
+++ rust/value-type/target/debug/value-type.d
@@ -0,0 +1,1 @@
+C:\Users\jjy\server\rust\value-type\target\debug\value-type.exe: C:\Users\jjy\server\rust\value-type\src\main.rs
